372385ae46f33fea039c220d67626b13f585fc55
David Sttivend <david.sttivend@uruit.com>
2016-09-16 12:35:51 -0500

    Agregado metodos de listar archivos del drive, quick start de sheet values

---
+++ index.js
@@ -0,0 +1,132 @@
+'use strict';
+
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+const path = require('path');
+
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/sheets.googleapis.com-nodejs-quickstart.json
+
+var SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'];
+var TOKEN_DIR = path.join(__dirname, '/.credentials/');
+var TOKEN_PATH = TOKEN_DIR + 'token.json';
+
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', (err, content) => {
+    if (err) {
+        console.log('Error loading client secret file: ' + err);
+        return;
+    }
+    // Authorize a client with the loaded credentials, then call the
+    // Google Sheets API.
+    authorize(JSON.parse(content), listFiles);
+});
+
+const authorize = (credentials, callback) => {
+    var clientSecret = credentials.installed.client_secret;
+    var clientId = credentials.installed.client_id;
+    var redirectUrl = credentials.installed.redirect_uris[0];
+    var auth = new googleAuth();
+    var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        if (err) {
+            getNewToken(oauth2Client, callback);
+        } else {
+            oauth2Client.credentials = JSON.parse(token);
+            callback(oauth2Client);
+        }
+    });
+};
+
+const getNewToken = (oauth2Client, callback) => {
+    var authUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES
+    });
+    console.log('Authorize this app by visiting this url: ', authUrl);
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+        rl.close();
+        oauth2Client.getToken(code, (err, token) => {
+            if (err) {
+                console.log('Error while trying to retrieve access token', err);
+                return;
+            }
+            oauth2Client.credentials = token;
+            storeToken(token);
+            callback(oauth2Client);
+        });
+    });
+};
+
+const storeToken = (token) => {
+    try {
+        fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+        if (err.code != 'EEXIST') {
+            throw err;
+        }
+    }
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+    console.log('Token stored to ' + TOKEN_PATH);
+};
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+const listFiles = (auth) => {
+    let service = google.drive('v3');
+    let options = {
+        auth: auth,
+        pageSize: 50,
+        fields: "nextPageToken, files(id, name)"
+    };
+    service.files.list(options, (err, res) => {
+        if (err) {
+            console.log(`The API returned an error: ${err}`);
+            return;
+        }
+        let files = res.files;
+        if (files.length == 0) {
+            console.log(`No files found`);
+            return;
+        }
+        files.forEach(file => {
+            console.log(`${file.name}\n${file.id} \n\n`);
+        });
+    });
+};
+
+
+const listMajors = (auth) => {
+    var sheets = google.sheets('v4');
+    sheets.spreadsheets.values.get({
+        auth: auth,
+        spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+        range: 'Class Data!A2:E',
+    }, (err, response) => {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+        }
+        var rows = response.values;
+        if (rows.length == 0) {
+            console.log('No data found.');
+        } else {
+            console.log('Name, Major:');
+            for (var i = 0; i < rows.length; i++) {
+                var row = rows[i];
+                // Print columns A and E, which correspond to indices 0 and 4.
+                console.log('%s, %s', row[0], row[4]);
+            }
+        }
+    });
+};
